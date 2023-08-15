2aac12a8411023b4b9782445e9713ed41895d58e
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-08-15 12:50:05 +0300

    fix

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { contactsReducer } from "./contactsReducer";
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer: {
+        contacts: contactsReducer,
+    },
+    
+});
+
