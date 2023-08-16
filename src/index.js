70438ec943aff7e358a0ab7f3328c9794c400057
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2023-08-16 09:44:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from './App';
 
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
 
+        <BrowserRouter>
         <App />
+        </BrowserRouter>
     
     </Provider>
   </React.StrictMode>
